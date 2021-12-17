1c8321b12de5dd7206ebce9eab5cddecef327104
nealgarcia <nathnlgarcia@gmail.com>
2021-12-17 11:22:34 -0600

    imported update component to app.js and created links/routes

--- employee-searcher-frontend/src/App.js
+++ employee-searcher-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import { useEffect } from "react";
 import Splash from "./components/Splash";
 import NewEmployee from "./components/NewEmployee";
+import UpdateEmployee from "./components/UpdateEmployee"
 import EmployerHome from "./components/EmployerHome";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Route path="/" exact component={Splash} />
       <Route path="/new" exact component={NewEmployee} />
+      <Route path="/update" exact component={UpdateEmployee} />
       <Route path="/search" exact component={EmployerHome} />
       <Footer />
     </div>
