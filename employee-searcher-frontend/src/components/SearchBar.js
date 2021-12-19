a6a64adf16f2bbe63f075791c727c723116bf7ad
nealgarcia <nathnlgarcia@gmail.com>
2021-12-19 11:11:11 -0600

    search state is set and logs to console on user input of search bar

--- employee-searcher-frontend/src/components/SearchBar.js
+++ employee-searcher-frontend/src/components/SearchBar.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useState } from 'react'; 
 
 function SearchBar(props) {
+    const [search, setSearch] = useState("")
+
+    function handleChange(event) {
+        setSearch(event.target.value)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(search)
+    }
+
+    
     return (
         <div className='component'>
-            <input placeholder='what do you need?'></input>
-            <button>search</button>
+            <form className = "searchForm" onSubmit = {handleSubmit}>
+                <input 
+                    placeholder='what do you need?'
+                    type = "text"
+                    onChange = {handleChange}
+                 />
+                <button type = "submit">search</button>
+            </form>
         </div>
     );
 }
