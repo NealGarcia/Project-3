09a46dbe978a739389c8bb0bf06f41e262eea833
julianadepp <julianadepp7@gmail.com>
2021-12-21 14:17:57 -0500

    create method functions

--- employee-searcher-frontend/src/components/NewEmployee.js
+++ employee-searcher-frontend/src/components/NewEmployee.js
@@ -1,80 +1,148 @@
-import { Link } from "react-router-dom"
- 
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import React from "react";
 
 function NewEmployee(props) {
+   const [newEmployee, setNewEmployee] = useState({});
+  // const [name, setName] = useState(undefined);
+
+
+  async function call(url = "", data = {}) {
+    const options = {
+      method: "Post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+      mode: "cors",
+    };
+    const Employee = await fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => {
+        setNewEmployee(json)
+        console.log("Success:", newEmployee);
+      });
+    }
+  console.log( newEmployee)
+
+
+  function handleChange(e) {
+    setNewEmployee({...newEmployee,[e.target.id]:e.target.value});
+    console.log(newEmployee)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    call('http://localhost:8000/api/', newEmployee).then((emp) => console.log(emp))
+  }
   return (
     <div className="component">
-
       <div className="formContainer">
-        <form className="newEmployeeForm">
-        <Link to={"/"} id="return">
-          <p class="return">Go Back</p>
-        </Link>
+        <form className="newEmployeeForm" onSubmit={handleSubmit}>
+          <Link to={"/"} id="return">
+            <p className="return">Go Back</p>
+          </Link>
           <h2>Create a New Profile</h2>
           <h3>Personal Information</h3>
-          <div class="form-group">
-            <input placeholder="Name:" class="input-control" />
-            <input placeholder="Age:" class="input-control" />
+          <div className="form-group">
+            <input
+              required
+              placeholder="Name:"
+              className="input-control"
+              name="name"
+              // value={name}
+              id="name"
+              onChange={handleChange}
+            />
+            <input
+              required
+              placeholder="Age:"
+              className="input-control"
+              name="age"
+              id="age"
+              onChange={handleChange}
+            />
           </div>
 
-          <div class="form-group">
-
-            <input placeholder="Profile Image URL:" class="input-control"></input>
+          <div className="form-group">
+            <input
+              placeholder="Profile Image URL:"
+              className="input-control"
+              name="image[url]"
+              id="url"
+            ></input>
           </div>
 
-          <div class="form-group">
-            <input placeholder="Address" class="input-control" />
+          <div className="form-group">
+            <input
+              placeholder="Address"
+              className="input-control"
+              id="address"
+            />
           </div>
 
-          <div class="form-group">
-            <input placeholder="City" class="input-control" />
-            <input class="input-control" placeholder="State" />
-            <input class="input-control" placeholder="ZIP" />
+          <div className="form-group">
+            <input placeholder="City" className="input-control" id="city" />
+            <input className="input-control" placeholder="State" id="state" />
+            <input className="input-control" placeholder="ZIP" id="zip" />
           </div>
 
-            <h3>Work History</h3>
-            <div class="form-group">
-            <input class="input-control" placeholder="Job Title" />
-            <input class="input-control" placeholder="Company" />
+          <h3>Work History</h3>
+          <div className="form-group">
+            <input
+              className="input-control"
+              placeholder="Job Title"
+              id="title"
+            />
+            <input
+              className="input-control"
+              placeholder="Company"
+              id="company"
+            />
           </div>
 
-            <h3>Contact Info</h3>
-            <div class="form-group">
-            <input class="input-control" placeholder="Phone Number" />
-            <input class="input-control" placeholder="Email Address" />
+          <h3>Contact Info</h3>
+          <div className="form-group">
+            <input
+              className="input-control"
+              placeholder="Phone Number"
+              id="phone"
+            />
+            <input
+              className="input-control"
+              placeholder="Email Address"
+              id="email"
+            />
           </div>
 
-            <h3>Availability</h3>
-            <p>Choose the days you are available:</p>
-            <div class = "checkboxes">
-                <label for="mon">Mon.</label>
-                <input type="checkbox" id="mon" name="mon"/>
+          <h3>Availability</h3>
+          <p>Choose the days you are available:</p>
+          <div className="checkboxes">
+            <label htmlFor="mon">Mon.</label>
+            <input type="checkbox" id="mon" name="mon" />
 
-                <label for="tues">Tues.</label>
-                <input type="checkbox" id="tues" name="tues"/>
+            <label htmlFor="tues">Tues.</label>
+            <input type="checkbox" id="tues" name="tues" />
 
-                <label for="wed">Wed.</label>
-                <input type="checkbox" id="wed" name="wed"/>
+            <label htmlFor="wed">Wed.</label>
+            <input type="checkbox" id="wed" name="wed" />
 
-                <label for="thu">Thu.</label>
-                <input type="checkbox" id="thu" name="thu"/>
+            <label htmlFor="thu">Thu.</label>
+            <input type="checkbox" id="thu" name="thu" />
 
-                <label for="fri">Fri.</label>
-                <input type="checkbox" id="fri" name="fri"/>
+            <label htmlFor="fri">Fri.</label>
+            <input type="checkbox" id="fri" name="fri" />
 
-                <label for="sat">Sat.</label>
-                <input type="checkbox" id="sat" name="sat"/>
+            <label htmlFor="sat">Sat.</label>
+            <input type="checkbox" id="sat" name="sat" />
 
-                <label for="sun">Sun.</label>
-                <input type="checkbox" id="sun" name="sun"/>
-            </div>
-          <div class="checkboxes">
-            <button className = "createButton">Create</button>
+            <label htmlFor="sun">Sun.</label>
+            <input type="checkbox" id="sun" name="sun" />
+          </div>
+          <div className="checkboxes">
+            <button className="createButton">Create</button>
           </div>
         </form>
-            <Link to = {"/update"}>Already have a profile? Click Here!</Link>
-        </div>
+        <Link to={"/update"}>Already have a profile? Click Here!</Link>
+      </div>
     </div>
   );
 }
