e883639a659d198ed3e1fd121a5695995a3a1d8f
nealgarcia <nathnlgarcia@gmail.com>
2021-12-20 22:31:39 -0600

    data is passed into details component using useContext and useLocatin

--- employee-searcher-frontend/src/components/Details.js
+++ employee-searcher-frontend/src/components/Details.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom'
+
 
 function Details(props) {
+    const [data, setData] = useState([]);
+    const location = useLocation()
+    const url = ``;
+  
+    useEffect(() => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch(console.err);
+    }, []);
+
+    console.log("in details component: " + location.state)
+
     return (
         <div>
+
             Details component
+
+
         </div>
     );
 }
